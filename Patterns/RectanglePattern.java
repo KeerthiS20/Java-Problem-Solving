bd0fb3cb5a792a2d9f941106f693c85c297ca917
KeerthiS20 <keerthisv20@gmail.com>
2025-08-06 20:24:48 +0530

    changes

---
+++ Patterns/RectanglePattern.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class RectanglePattern {
+
+    public static void main(String[] args) {
+    
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
